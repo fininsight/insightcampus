93fd36d622cbbfde36af84366f1912e1dfb43b75
mona <kjwook7522@gmail.com>
2020-05-20 19:53:26 +0900

    user 테이블 화면 만들기

--- src/app/admin/page-user/page-user.module.ts
+++ src/app/admin/page-user/page-user.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageUserComponent } from './page-user.component';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { UserService } from '../core/services/user.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    NgZorroAntdModule
+    NgZorroAntdModule,
+    FormsModule
+  ],
+  providers: [
+    UserService
   ],
   declarations: [
     PageUserComponent

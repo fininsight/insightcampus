68173b225c5b812581f1b99f63194daadfffb50f
mona <kjwook7522@gmail.com>
2020-05-29 23:56:18 +0900

    강의관리 화면 완성

---
+++ src/app/admin/core/models/class.ts
@@ -0,0 +1,11 @@
+export class Class {
+  class_seq: Number;
+  class_nm: String;
+  teacher: Number;
+  duration: Number;
+  duration_nm: String;
+  thumbnail: String;
+  price: Number;
+  real_price: Number;
+  template: String;
+}
---
+++ src/app/admin/page-class/page-class.component.ts
@@ -0,0 +1,114 @@
+import { Component, OnInit } from '@angular/core';
+import { ClassService } from '../core/services/class.service';
+import { DataTable } from '../core/models/datatable';
+import { Class } from '../core/models/class'
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzModalService } from 'ng-zorro-antd/modal';
+
+@Component({
+  selector: 'app-page-class',
+  templateUrl: './page-class.component.html',
+  styleUrls: ['./page-class.component.css']
+})
+export class PageClassComponent implements OnInit {
+
+  classes = new DataTable();
+
+  selectedClass: Class = new Class();
+
+  popupClass: Class = new Class();
+
+  isClassAdd = false;
+  isClassUpdate = false;
+  
+  classLoading = true;
+
+  sortValue: string | null = null;
+  sortKey: string | null = null;
+
+  sort(sort: { key: string; value: string }): void {
+    this.sortKey = sort.key;
+    this.sortValue = sort.value;
+    // this.searchData();
+  }
+
+  constructor(private userService: ClassService,
+              private modal: NzModalService,
+              private message: NzMessageService) { 
+                this.classes.pageNumber = 1;
+                this.classes.size = 10;
+          
+                this.getClass();
+  }
+
+  ngOnInit(): void {
+  }
+
+  getClass() {
+    this.userService.getClasses(this.classes).subscribe(data => {
+      this.classes = data;
+      this.classLoading = false;
+      this.selectedClass = new Class();
+    });
+  }
+
+  selectClass(param) {
+    this.selectedClass = param;
+  }
+  
+  classAdd() {
+    this.popupClass = new Class();
+    this.isClassAdd= true;
+  }
+  
+  classAddOk() : void {
+    this.userService.addClass(this.popupClass).subscribe(data => {
+      this.getClass();
+      this.isClassAdd = false;
+      this.message.create('success', '사용자 추가가 완료되었습니다.');
+    })
+  }
+  
+  classUpdate() {
+    this.popupClass = new Class();
+    this.popupClass.class_seq = this.selectedClass.class_seq;
+    this.popupClass.class_nm = this.selectedClass.class_nm;
+    this.popupClass.teacher = this.selectedClass.teacher;
+    this.popupClass.duration = this.selectedClass.duration;
+    this.popupClass.duration_nm = this.selectedClass.duration_nm;
+    this.popupClass.thumbnail = this.selectedClass.thumbnail;
+    this.popupClass.price = this.selectedClass.price;
+    this.popupClass.real_price = this.selectedClass.real_price;
+    this.popupClass.template = this.selectedClass.template;
+    this.isClassUpdate = true;
+  }
+  
+  classUpdateOk() : void {
+    this.userService.updateClass(this.popupClass, this.popupClass.class_seq).subscribe(data => {
+      this.getClass();
+      this.isClassUpdate = false;
+      this.message.create('success', '사용자 수정이 완료되었습니다.');
+    })
+  }
+  
+  classDelete() {
+    this.modal.confirm({
+      nzTitle: '사용자를 삭제하시겠습니까?',
+      nzContent: '',
+      nzOkText: '예',
+      nzOkType: 'danger',
+      nzCancelText: '아니요',
+      nzOnOk: () => {
+        this.userService.deleteClass(this.selectedClass.class_seq).subscribe(data => {
+          this.getClass();
+          this.message.create('success', '사용자 삭제가 완료되었습니다.');
+        })
+      }
+    });
+  }
+  
+  popupCancel() : void {
+    this.isClassAdd = false;
+    this.isClassUpdate = false;
+  }
+}

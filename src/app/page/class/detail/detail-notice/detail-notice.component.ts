b507c619c136c1e6356baa877965bc5c3567f512
kjwook7522 <kjwook7522@gmail.com>
2020-07-22 15:33:22 +0900

    강의공지 api연동 작업완료 / 더보기 기능 완료

--- src/app/page/class/detail/detail-notice/detail-notice.component.ts
+++ src/app/page/class/detail/detail-notice/detail-notice.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ClassNoticeService } from 'src/app/admin/core/services/class-notice.service';
+import { ClassNotice } from 'src/app/admin/core/models/class-notice';
+import { DataTable } from '../../../core/models/datatable';
+import { NzModalService, NzModalRef } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-detail-notice',
@@ -7,9 +12,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailNoticeComponent implements OnInit {
 
-  constructor() { }
+  public class_seq: any;
+
+  notices: DataTable = new DataTable();
+  popupNotice: ClassNotice = new ClassNotice();
+  buttonLoading = false;
+
+  tplmodal?: NzModalRef;
+
+  constructor(private classNoticeService: ClassNoticeService,
+              private route: ActivatedRoute,
+              private modal: NzModalService) {
+    
+    route.params.subscribe(val => {
+      this.class_seq = val.class_seq;
+    });
+
+    this.notices.pageNumber = 1;
+    this.notices.size = 10;
+
+    this.getClassNotices();
+  }
 
   ngOnInit() {
   }
 
+  getClassNotices(){
+    this.buttonLoading = true;
+    this.classNoticeService.getClassNotices(this.notices, this.class_seq).subscribe(data => {
+      this.notices = data;
+      console.log(this.notices)
+      if (this.isEndOfPage()) this.updateFinishBtn();
+      this.buttonLoading = false;
+    })
+  }
+
+  getMoreNotices() {
+    this.buttonLoading = true;
+    if (this.notices.pageNumber < this.notices.totalPages) {
+      this.notices.pageNumber += 1;
+      this.classNoticeService.getClassNotices(this.notices, this.class_seq).subscribe(data => {
+        this.notices.data = this.notices.data.concat(data.data);
+        if (this.isEndOfPage()) this.updateFinishBtn();
+        this.buttonLoading = false;
+      })
+    } else {
+      throw new Error('Invalid page number!');
+    }
+  }
+
+  isEndOfPage() {
+    if (this.notices.pageNumber === this.notices.totalPages)
+      return true;
+    else if (!this.notices.data.length)
+      return true;
+    else
+      return false;
+  }
+
+  updateFinishBtn() {
+    const btn = document.querySelector('.more-button');
+    btn.setAttribute('disabled', '');
+    btn.querySelector('span').innerText = '끝';
+  }
+
+  popupNoticeOpen(header: TemplateRef<{}>, body: TemplateRef<{}>, footer: TemplateRef<{}>){
+    this.popupNotice = new ClassNotice();
+    this.tplmodal = this.modal.create({
+      nzWidth: 600,
+      nzTitle: header,
+      nzContent: body,
+      nzFooter: footer,
+      nzClosable: false,
+    });
+  }
+
+  popupNoticeClose() {
+    this.tplmodal.destroy();
+  }
 }

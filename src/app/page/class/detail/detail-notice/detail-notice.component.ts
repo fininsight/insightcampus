922ab1c34d567a1838ed5ba36b30014766aa4eb1
kjwook7522 <kjwook7522@gmail.com>
2020-07-22 10:27:31 +0900

    detail-notice component 생성

---
+++ src/app/page/class/detail/detail-notice/detail-notice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-notice',
+  templateUrl: './detail-notice.component.html',
+  styleUrls: ['./detail-notice.component.css']
+})
+export class DetailNoticeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

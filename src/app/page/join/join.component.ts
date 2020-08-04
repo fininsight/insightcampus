ff4f66af5662ea04a6f10cb8665421f974a44501
kjwook7522 <kjwook7522@gmail.com>
2020-08-04 14:20:12 +0900

    틀 수정 및 input 테스트

--- src/app/page/join/join.component.ts
+++ src/app/page/join/join.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../core/models/user';
 
 @Component({
   selector: 'app-join',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JoinComponent implements OnInit {
 
+  inputUser: User = new User();
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.inputUser = new User()
+  }
+
+  joinOk() {
+    console.log(this.inputUser);
   }
 
 }

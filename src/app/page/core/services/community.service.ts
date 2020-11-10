516281bd24cd590e2286edf7ccbb31c0dbf73d39
kjwook7522 <kjwook7522@gmail.com>
2020-11-11 03:01:06 +0900

    community 서비스 및 모델 생성

---
+++ src/app/page/core/services/community.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { DataTable } from '../models/datatable';
+import { map, retry, catchError } from 'rxjs/operators';
+import { Community } from '../models/community';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommunityService {
+
+  baseUrl = environment.apiUrl;
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  getBoards(dataTable: DataTable): Observable<DataTable> {
+    let searchText = '';
+
+    if (dataTable.search != null) {
+      searchText = JSON.stringify(dataTable.search);
+      searchText = encodeURI(searchText);
+    }
+
+    return this.http.get<DataTable>(this.baseUrl + 'community/' + dataTable.size + '/' + dataTable.pageNumber + '/' + searchText)
+    .pipe(
+      retry(1),
+      catchError(this.errorHandl)
+    );
+  }
+
+  getBoard(board_seq: number): Observable<Community> {
+    return this.http.get<Community>(this.baseUrl + 'community/' + board_seq)
+    .pipe(
+      retry(1),
+      catchError(this.errorHandl)
+    );
+  }
+
+  // Error handling
+  errorHandl(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+}

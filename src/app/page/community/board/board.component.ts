ee81a586715c8982c58cc520d0c9a34733f08aad
kjwook7522 <kjwook7522@gmail.com>
2020-11-19 02:02:53 +0900

    board 기본 세팅

--- src/app/page/community/board/board.component.ts
+++ src/app/page/community/board/board.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CommunityService } from '../../core/services/community.service';
+import { Community } from '../../core/models/community';
 
 @Component({
   selector: 'app-board',
@@ -9,15 +11,20 @@ import { ActivatedRoute } from '@angular/router';
 export class BoardComponent implements OnInit {
 
   public board_seq: any;
+  public board: Community = new Community();
 
-  constructor(private route: ActivatedRoute) { 
+  constructor(private communityService: CommunityService,
+              private route: ActivatedRoute) { 
+    
     route.params.subscribe(val => {
       this.board_seq = val.board_seq;
     });
+
+    this.communityService.getBoard(this.board_seq).subscribe(data => {
+      this.board = data;
+    })
   }
 
-  ngOnInit() {
-    console.log(this.board_seq);
-  }
+  ngOnInit() { }
 
 }

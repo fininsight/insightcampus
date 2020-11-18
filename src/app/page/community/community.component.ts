a6b929c9334bca3e1dd206e395995927d155402d
kjwook7522 <kjwook7522@gmail.com>
2020-11-19 00:52:34 +0900

    게시판 서버연동

--- src/app/page/community/community.component.ts
+++ src/app/page/community/community.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataTable } from '../core/models/datatable';
+import { CommunityService } from '../core/services/community.service';
 
 @Component({
   selector: 'app-community',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommunityComponent implements OnInit {
 
-  constructor() { }
+  boards = new DataTable();
+
+  constructor(private communityService: CommunityService,
+              private router: Router) {
+    this.boards.pageNumber = 1;
+    this.boards.size = 20;
+
+    this.communityService.getBoards(this.boards).subscribe(data => {
+      this.boards = data;
+    });
+  }
 
   ngOnInit() {
+  }
+
+  gotoPage(board_seq) {
+    this.router.navigate(['/board/' + board_seq]);
+  }
+
+  convertDateType(dateStr) {
+    const dateDate = new Date(dateStr);
+    let dateConvert = "";
+
+    dateConvert += (dateDate.getFullYear() + ". ");
+    dateConvert += (dateDate.getMonth()+1 + ". ");
+    dateConvert += dateDate.getDate();
+
+    return dateConvert;
   }
 
 }

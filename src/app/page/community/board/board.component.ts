52cf60e361819131d4aa7e72c187ac7fc6962f8c
kjwook7522 <kjwook7522@gmail.com>
2020-11-26 04:10:27 +0900

    글 에디터 연동 성공

--- src/app/page/community/board/board.component.ts
+++ src/app/page/community/board/board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CommunityService } from '../../core/services/community.service';
 import { Community } from '../../core/models/community';
@@ -12,6 +12,10 @@ export class BoardComponent implements OnInit {
 
   public board_seq: any;
   public board: Community = new Community();
+  public admin = false;
+  public templates = ["<div>hello</div>", "<div>good</div>"];
+
+  @ViewChildren('class') classes: QueryList<ElementRef>;
 
   constructor(private communityService: CommunityService,
               private route: ActivatedRoute) { 
@@ -25,6 +29,46 @@ export class BoardComponent implements OnInit {
     })
   }
 
+  public option =  {
+    toolbarInline: true,
+    charCounterCount: false,
+    toolbarVisibleWithoutSelection: true,
+
+    events : {
+      initialized: (editor) => {
+        // editor._editor.edit.off();
+      },
+      'editable.contentChanged': (editor) => {
+        console.log('컨텐츠 체인지');
+        console.log(editor);
+      },
+      focus: (editor) => {
+        // console.log(editor);
+      },
+      contentChanged: () => {
+        try {
+          for (let i = 0; i < this.classes['_results'].length; i++) {
+            this.templates[i] = this.classes['_results'][i].nativeElement.children[0].children[0].innerHTML;
+          }
+        } catch {
+
+        }
+      }
+    },
+  };
+
   ngOnInit() { }
+
+  adminAble() {
+    this.admin = true;
+  }
+
+  adminDisable() {
+    this.admin = false;
+  }
+
+  test() {
+    console.log(this.templates);
+  }
 
 }
--- src/app/page/community/community.module.ts
+++ src/app/page/community/community.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
 import { BoardComponent } from './board/board.component';
+
+// Import all Froala Editor plugins.
+import 'froala-editor/js/plugins.pkgd.min.js';
+
+// Import a single Froala Editor plugin.
+import 'froala-editor/js/plugins/align.min.js';
+
+// Import a Froala Editor language file.
+import 'froala-editor/js/languages/de.js';
+
+// Import a third-party plugin.
+import 'froala-editor/js/third_party/font_awesome.min';
+import 'froala-editor/js/third_party/image_tui.min';
+import 'froala-editor/js/third_party/spell_checker.min';
+import 'froala-editor/js/third_party/embedly.min';
 
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FroalaEditorModule.forRoot(),
+    FroalaViewModule.forRoot(),
   ],
   declarations: [
     BoardComponent

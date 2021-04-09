8e93cd74e2c1cb4495adf64b0c87ee968e533c5e
kjwook7522 <kjwook7522@gmail.com>
2021-04-09 16:04:36 +0900

    주문관리 화면, 추가 수정 삭제 팝업 화면 완성

--- src/app/admin/page-order-manage/page-order-manage.component.ts
+++ src/app/admin/page-order-manage/page-order-manage.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DataTable } from '../core/models/datatable';
+import { CodeService } from '../core/services/code.service';
+import { Order } from '../core/models/order';
+import { Code } from '../core/models/code';
 
 @Component({
   selector: 'app-page-order-manage',
@@ -6,10 +10,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-order-manage.component.css']
 })
 export class PageOrderManageComponent implements OnInit {
+  
+  orders = new DataTable();
+  
+  selectedOrder: Order = new Order();
+  
+  popupOrder: Order = new Order();
+  
+  orderLoading = false;
 
-  constructor() { }
+  isOrderAdd = false;
+  isOrderUpdate = false;
+
+  orderCode: Array<Code> = [];
+  
+  sortValue: string | null = null;
+  sortKey: string | null = null;
+
+  filter = {
+    order_date:'',
+    address: '',
+  };
+
+  sort(sort: { key: string; value: string }): void {
+    this.sortKey = sort.key;
+    this.sortValue = sort.value;
+  }
+
+  constructor(private codeService: CodeService) {
+                this.getCodes();
+  }
 
   ngOnInit() {
+  }
+
+  getOrderFilter() {
+
+  }
+
+  getCodes() {
+    this.codeService.getCodes('order_type').subscribe(data => {
+      this.orderCode = data;
+    })
+  }
+
+  orderAdd() {
+    this.popupOrder = new Order();
+    this.isOrderAdd= true;
+  }
+
+  orderAddOk() {
+    console.log(this.popupOrder)
+  }
+
+  orderUpdate() {
+    this.popupOrder = new Order();
+    this.popupOrder.order_id = this.selectedOrder.order_id;
+    this.popupOrder.order_date = this.selectedOrder.order_date;
+    this.popupOrder.order_price = this.selectedOrder.order_price;
+    this.isOrderUpdate = true;
+  }
+
+  orderUpdateOk() {
+
+  }
+
+  orderDelete() {
+
+  }
+
+  popupCancel(): void {
+    this.isOrderAdd = false;
+    this.isOrderUpdate = false;
   }
 
 }
